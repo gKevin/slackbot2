a77bcb71ab71f52c6a96f3f87c32a11764693706
Kevin <hello@kevingaudet.eu>
2020-12-11 18:31:45 +0100

    Fix word matching by taking into account punctuation and space Add respond to a random user

--- slackbot2.js
+++ slackbot2.js
@@ -4,26 +4,82 @@ let token = require('./token.json').token
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const botName = 'SlackBot2';
+function pickRandomArrayElt(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function pickRandomUsername(channel) {
+    const botName = bot.user.username;
+
+    var members = Array.from(channel.members.values());
+    members = members.filter(member => member.user.username !== botName);
+
+    const randMember = pickRandomArrayElt(members);
+
+    if(randMember != undefined) {
+        return randMember.user.username;
+    }
+
+    return '';
+}
+
+function expandResponseVariables(message, reply) {
+    const randomUserTag = '\{randomUser\}';
+    const userTag       = '\{user\}'
+
+    var botResponse = reply.replace(userTag, message.author.username);
+
+    if(botResponse.includes(randomUserTag)) {
+        const randomUser = pickRandomUsername(message.channel);
+        botResponse = botResponse.replace(randomUserTag, randomUser);
+    }
+
+    return botResponse;
+}
+
+function isPunctuation(char) {
+    return /[\s\W\d]/.test(char);
+}
+
+function matchesTrigger(messageContent, trigger) {
+    const subStrIndex = messageContent.toLowerCase().indexOf(trigger.toLowerCase());
+
+    if(subStrIndex >= 0) {
+        const precededBySpace = (subStrIndex === 0 
+            || isPunctuation(messageContent.charAt(subStrIndex - 1)));
+        const followedBySpace = (subStrIndex + trigger.length === messageContent.length) 
+            || isPunctuation(messageContent.charAt(subStrIndex + trigger.length));
+
+        return precededBySpace && followedBySpace;
+    }
+
+    return false;
+}
+
+function findTriggerIndex(messageContent) {
+    return autoResponses.findIndex(autoresp => 
+        autoresp.triggers.find(trigger => matchesTrigger(messageContent, trigger)));
+}
+
+function respond(message, triggerIndex) {
+    const responses = autoResponses[triggerIndex].responses;
+    if(responses.length > 0) {
+        var botResponse = pickRandomArrayElt(responses);
+        botResponse = expandResponseVariables(message, botResponse);
+
+        message.channel.send(botResponse);
+    }
+}
 
 bot.on('message', async message => {
+    const author  = message.author.username;
+    const botName = bot.user.username;
 
-    const author = message.author.username;
+    if (author !== botName && message.channel.name ==='test') {
+        const triggerIndex = findTriggerIndex(message.content);
 
-    if (author !== botName) {
-        const respIndex = autoResponses.findIndex(autoresp => 
-            autoresp.triggers.find(trigger => 
-                message.content.toLowerCase().includes(trigger.toLowerCase())));
-
-        if(respIndex > -1) {
-            const responses = autoResponses[respIndex].responses;
-            if(responses.length > 0)
-            {
-                var botResponse = responses[Math.floor(Math.random() * responses.length)];
-                botResponse = botResponse.replace('\{user\}', author);
-
-                message.channel.send(botResponse);
-            }
+        if(triggerIndex > -1) {
+            respond(message, triggerIndex);
         }
     }
 });
